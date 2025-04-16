bdc19fba9b91ca0f17c673922da227067bce7573
fantasticFANmagician <fantasticmagicianfan@gmail.com>
2025-04-16 16:51:27 -0400

    project initialization

---
+++ public/firebaseConfig.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBz3XY4T2EmXRcZJJycgUVEXcu_BMwon1o",
+  
+    authDomain: "localbites-9ea1f.firebaseapp.com",
+  
+    projectId: "localbites-9ea1f",
+  
+    storageBucket: "localbites-9ea1f.firebasestorage.app",
+  
+    messagingSenderId: "680689209427",
+  
+    appId: "1:680689209427:web:245c7a8113596dd38e36a6",
+  
+    measurementId: "G-SX897YRJPV"
+  
+  };
+
+  export default firebaseConfig;
+  
